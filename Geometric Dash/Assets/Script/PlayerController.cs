55ee4315e19952baeb4a5cf1819229e626831fbc
Waignier Stéphane <97592098+Stephwgr@users.noreply.github.com>
2023-02-27 01:44:27 +0100

    Player move && Jumping but no rotate in jumping

--- Geometric Dash/Assets/Script/PlayerController.cs
+++ Geometric Dash/Assets/Script/PlayerController.cs
@@ -1,39 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector3 _dirPlayer;
+    [Header("Player")]
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
+    private Rigidbody2D _rb;
+    private BoxCollider2D _coll;
+    //------------------------------------------------
 
-    private Rigidbody _rb;
+    [Header("Rotation Cube")]
+    [SerializeField] private GameObject _cube;
+    public Vector3 _currentEulerAngles;
+    public float _speedRotate;
+    public float _angle;
+    //------------------------------------------------
 
-    void Start()
+    [Header("Ground Check")]
+    public float _hitDistance;
+    [SerializeField] LayerMask _groundMask;
+
+    private void Start() 
     {
-        _rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody2D>();
+        _coll = GetComponent<BoxCollider2D>();
     }
 
-    
-    void Update()
+    void FixedUpdate()
     {
         Move();
+    }
+
+    private void Update() 
+    {
+        Debug.DrawRay(transform.position, Vector2.down * _hitDistance, Color.red);
         Jump();
+
+        // _cube.transform.Rotate(0,0, zAngle, Space.World);
+
+        
+
     }
 
     public void Move()
-    {
-        transform.Translate(_speed * Time.deltaTime, 0, 0);
-
+    { 
+        transform.position += new Vector3(_speed * Time.deltaTime, 0, 0);
     }
 
     public void Jump() 
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            _rb.AddForce(Vector2.up * _jumpForce, ForceMode.Impulse);
-        }    
+            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
 
+            // Rotation Cube
+            _currentEulerAngles += new Vector3(0,0,_angle) * Time.deltaTime * _speedRotate;
+            _cube.transform.localEulerAngles = _currentEulerAngles;
+
+        }    
     }
+
+    private bool IsGrounded()
+    {
+        
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(transform.position, Vector2.down, _hitDistance, _groundMask);
+
+        return hit;
+    } 
 }
