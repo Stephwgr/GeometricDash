fc4afb86f6e0f0e676b23972795ce0bfe1db9c0e
Waignier Stéphane <97592098+Stephwgr@users.noreply.github.com>
2023-03-11 04:08:54 +0100

    pS ne marche pas

--- Geometric Dash/Assets/Script/PlayerController.cs
+++ Geometric Dash/Assets/Script/PlayerController.cs
@@ -8,19 +8,29 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _jumpForce;
     private Rigidbody2D _rb;
     private BoxCollider2D _coll;
-    
+
     [Header("Rotation Cube")]
     public float _duration;
-  
+    public float _angle;
+
     [Header("Ground Check")]
     public float _hitDistance;
     [SerializeField] LayerMask _groundMask;
+
+    [Header("Particles System")]
+    public ParticleSystem ps_SparkGround;
+    public float particleOffsetX;
+    public float particleOffsetY;
+    
+
 
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _coll = GetComponent<BoxCollider2D>();
+
+        
     }
 
     void FixedUpdate()
@@ -38,25 +48,41 @@ public class PlayerController : MonoBehaviour
     public void Move()
     {
         transform.position += new Vector3(_speed * Time.deltaTime, 0, 0);
+
     }
 
     public void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            
+
             _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-            
+
             //DoTween Rotation
-            transform.DORotate(new Vector3(0f,0f,transform.rotation.eulerAngles.z -90f), _duration);
-        }  
+            transform.DORotate(new Vector3(0f, 0f, transform.rotation.eulerAngles.z - _angle), _duration);
+        }
+
+        if (IsGrounded())
+        {
+
+            ps_SparkGround.Play();
+
+        }
+        else
+        {
+            ps_SparkGround.Stop();
+
+
+        }
     }
 
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
 
         RaycastHit2D hit;
-        hit = Physics2D.Raycast(transform.position, Vector2.down, _hitDistance, _groundMask);
+        hit = Physics2D.Raycast(transform.position, Vector2.down, _hitDistance, _groundMask);    
+
+
 
         return hit;
     }
